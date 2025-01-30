0389e9d049b6de4e9b15b7231648888a39d0b839
Kristian Toshev <145353070+kristoshev@users.noreply.github.com>
2025-01-30 08:18:47 -0600

    d

--- flatlistApp/app/index.tsx
+++ flatlistApp/app/index.tsx
@@ -3,20 +3,56 @@ import {
   View,
   StyleSheet,
   FlatList,
-  TouchableOpacity,
 } from "react-native";
-import colors from "../styles/colors";
+import { useState } from "react";
 import defaultStyles from "../styles/defaultStyles";
+import ListItemSeparator from "@/components/ListItemSeparator";
+import ListItem from "@/components/ListItem";
+import {dataType, DATA} from "@/data/appData";
+
 
 export default function Index() {
+
+  const [selectedId, setSelectedId] = useState<string>("1")
+
+  type dataType = {
+    id: string; // unique indentifier
+    title: string; // text we see in flatlist
+  };
+
+ 
+  
+  /* 
+  declaring a function called selectedList that receives
+  a param of type dataType that we will refer to as 'item'
+  I can access the values of item using dot notation 
+  */
+  
+
+  const handleRowPress = (item: dataType) => {
+    setSelectedId(item.id);
+    console.log("selected " + item.title);
+  }
+
   return (
     <View style={defaultStyles.container}>
       <View style={defaultStyles.titleContainer}>
-        <Text style={defaultStyles.title}>Insert Title Here</Text>
+        <Text style={defaultStyles.title}>Khushi Phushi Bhusi Boo</Text>
       </View>
       <View style={[defaultStyles.textContainer, { flex: 1 }]}>
         <View style={styles.flatlist}>
-          <Text>This is where our list will go</Text>
+          <FlatList
+           data = {DATA}
+           keyExtractor={(item: dataType) => item.id}
+           ItemSeparatorComponent={() => <ListItemSeparator color="blue"/>}
+           renderItem={({item}) => (
+            <ListItem 
+              item = {item}
+              isSelected = {item.id === selectedId}
+              onPress={handleRowPress}
+              />
+           )}
+          />
         </View>
       </View>
     </View>
@@ -26,15 +62,5 @@ export default function Index() {
 const styles = StyleSheet.create({
   flatlist: {
     alignItems: "center",
-  },
-  titleContainer: {
-    marginTop: 5,
-    width: 300,
-    borderTopLeftRadius: 15,
-    borderTopRightRadius: 15,
-  },
-  titleText: {
-    fontSize: 24,
-    padding: 10,
   },
 });
--- flatlistApp/components/ListItem.tsx
+++ flatlistApp/components/ListItem.tsx
@@ -1,11 +1,28 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
 import colors from "../styles/colors";
+import { dataType } from "@/data/appData";
+
+// add the declaration for the dataType
+// Later we will learn how to do this in one spot and import it
+
 
 // declare data type for props here
+type propsDataType = {
+  item: dataType;
+  isSelected: boolean; // this will determine how styling should look
+  onPress: (item: dataType) => void; //replace what was selectedList
+}
 
-const ListItem = () => {
-  return <View></View>;
+const ListItem: React.FC<propsDataType> = ({item, isSelected, onPress}) => {
+  return (
+    <TouchableOpacity onPress = {() => onPress(item)}>
+      <View style = {[styles.titleContainer, 
+        {backgroundColor: isSelected ? colors.primary : colors.secondary}]}>
+          <Text style = {[styles.titleText,{color: isSelected ? colors.text.light : colors.text.dark}]}>{item.title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 };
 
 export default ListItem;
@@ -23,5 +40,16 @@ const styles = StyleSheet.create({
   text: {
     color: colors.text.dark,
     fontSize: 20,
+  },
+  titleContainer: {
+    marginTop: 5,
+    width: 300,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+    backgroundColor: 'lightblue',
+  },
+  titleText: {
+    fontSize: 24,
+    padding: 10,
   },
 });
